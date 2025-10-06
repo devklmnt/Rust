d5b9320188c10fbdd57f37b6e78f108190158de1
Sebastien Klement <dev.klmnt@gmail.com>
2025-10-06 16:31:05 -0500

    hello world

---
+++ The Big Picture/hello_world/src/main.rs
@@ -0,0 +1,12 @@
+// List of commands:
+// cargo new hello_world
+// cd hello_world
+// cargo build 
+// cargo run 
+
+
+// definicion de una nueva funcion main
+fn main() {
+    println!("Hello, world!"); // print line (!) -> println es una macro
+}
+
